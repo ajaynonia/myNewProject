8d0fbd4b921189dfe1483f4526048418825b0c3f
ajaynonia <ajaynonia174@gmail.com>
2017-06-08 20:52:10 +0530

    Add files via upload

---
+++ startt.php
@@ -0,0 +1,225 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>HPE-Project</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <script type="text/javascript" src="//platform-api.sharethis.com/js/sharethis.js#property=5938358e2e01ff00121c6924&product=inline-share-buttons"></script>
+
+</head>
+<body>
+<style type="text/css">
+  
+
+  a{
+    font-size: 25px;
+  }
+  #myNavbar{
+    padding-left: 300px;
+    margin-top: 10px;
+  }
+  nav.navbar{
+      background-color: #9C27B0;
+    }
+	
+footer {
+      background-color: #555;
+      color: white;
+      padding:15px;
+	  height:300px;
+	  
+    }
+.fa {
+  padding: 20px;
+  font-size: 30px;
+  width: 50px;
+  text-align: center;
+  text-decoration: none;
+  margin: 5px 2px;
+}
+
+.fa:hover {
+    opacity: 0.7;
+}
+
+.fa-facebook {
+  background: #3B5998;
+  color: white;
+}
+.fa-twitter {
+  background: #55ACEE;
+  color: white;
+}
+
+.fa-google {
+  background: #dd4b39;
+  color: white;
+}
+
+</style>
+
+<nav class="navbar navbar-inverse" style="height: 80px">
+<nav class="navbar navbar-default navbar-fixed-top" style="height: 80px">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#" style="padding-left: 25px; margin-top: 10px; font-size: 25px">HPE-Project</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="none"><a href="#" style="color: white;"><span class="glyphicon glyphicon-home"></span></a></li>
+        <li class="dropdown">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Page 1-1</a></li>
+            <li><a href="#">Page 1-2</a></li>
+            <li><a href="#">Page 1-3</a></li>
+          </ul>
+        </li>
+        <li><a href="#">Page 2</a></li>
+        <li><a href="#">Page 3</a></li>
+         <form class="navbar-form navbar-left">
+        <div class="input-group">
+          <input type="text" class="form-control" placeholder="Search">
+          <div class="input-group-btn">
+            <button class="btn btn-default" type="submit">
+              <i class="glyphicon glyphicon-search"></i>
+            </button>
+          </div>
+        </div>
+      </form>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</nav>
+<div class="container-fluid">
+<div class="col-lg-8">
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+</div>
+</div>
+  
+<div class="container-fluid text-center">    
+  <div class="row content">
+    <div class="col-sm-1 sidenav">
+      
+    </div>
+    <div class="col-sm-10 text-left"> 
+      
+      <hr>
+      <div class="col-sm-5 text-left">
+      <h1>Welcome</h1>
+      <p>We are happy to help you</p>
+      </p>
+         <h1>Services</h1>
+            <ul class="list-group">
+              <li class="list-group-item">First item</li>
+              <li class="list-group-item">Second item</li>
+              <li class="list-group-item">Third item</li>
+              <li class="list-group-item">Fourth item</li>
+              <li class="list-group-item">Fifth item</li>
+              <li class="list-group-item">Sixth item</li>
+              <li class="list-group-item">Seventh item</li>
+              <li class="list-group-item">Eighth item</li>
+              <li class="list-group-item">Ninth item</li>
+              <li class="list-group-item">Third item</li>
+              <li class="list-group-item">Third item</li>
+              <li class="list-group-item">Third item</li>
+              <li class="list-group-item">Third item</li>
+              <li class="list-group-item">Third item</li>
+            </ul>
+      </div>
+<div class="col-sm-3 text-center">
+    <h1></h1>
+       <div class="card" style="width: 20rem;">
+            <img class="card-img-top" src="../../design/images/images (2).jpg" alt="Card image cap">
+                <div class="card-block">
+                    <h4 class="card-title">Card title</h4>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+       </div>
+</div>
+
+<div class="col-sm-3 text-center">
+    <h1></h1>
+       <div class="card" style="width: 20rem;">
+           <img class="card-img-top" src="../../design/images/images (2).jpg" alt="Card image cap">
+                <div class="card-block">
+                    <h4 class="card-title">Card title</h4>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+      </div>
+</div>
+
+<div class="col-sm-3 text-center">
+    <h1></h1>
+       <div class="card" style="width: 20rem;">
+           <img class="card-img-top" src="../../design/images/images (2).jpg" alt="Card image cap">
+                <div class="card-block">
+                    <h4 class="card-title">Card title</h4>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+      </div>
+</div>
+<div class="col-sm-3 text-center">
+    <h1></h1>
+       <div class="card" style="width: 20rem;">
+           <img class="card-img-top" src="../../design/images/images (2).jpg" alt="Card image cap">
+                <div class="card-block">
+                    <h4 class="card-title">Card title</h4>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+      </div>
+</div>
+<div class="col-sm-4 text-right">
+
+</div>
+<div class="col-sm-1 sidenav">
+</div>
+</div>
+</div>
+<br>
+<footer class="container-fluid text-center" >
+  <div class="row content">
+    <div class="col-sm-4 sidenav">
+      <h2>Contact Us</h2>
+      <br>
+      <p>NIT Durgapur</p>
+      <p>Durgapur Mahatma Gandhi Avenue-713209</p>
+      <p>West Bengal</p>
+      <p>Phone:9933737327</p>
+      </div>
+        <div class="col-sm-4 sidenav">
+      <h2>Motive</h2>
+    </div>
+    <div class="col-sm-4 sidenav">
+      <h2>We are here also</h2>
+      <div class="sharethis-inline-share-buttons"></div>
+
+  
+</footer>
+
+</body>
+</html>
